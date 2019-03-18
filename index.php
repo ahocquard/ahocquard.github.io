bd85f9a9996cdae0821147341d88faea936076bd
ahocquard <alexandre.hocquard@akeneo.com>
2019-03-18 11:20:13 +0100

    Initialize code coverage for the PIM

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <title>Code Coverage for /srv/pim/src/Akeneo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="_css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="_css/octicons.css" rel="stylesheet" type="text/css">
+  <link href="_css/style.css" rel="stylesheet" type="text/css">
+  <link href="_css/custom.css" rel="stylesheet" type="text/css">
+ </head>
+ <body>
+  <header>
+   <div class="container-fluid">
+    <div class="row">
+     <div class="col-md-12">
+      <nav aria-label="breadcrumb">
+       <ol class="breadcrumb">
+         <li class="breadcrumb-item active">/srv/pim/src/Akeneo</li>
+         <li class="breadcrumb-item">(<a href="dashboard.html">Dashboard</a>)</li>
+
+       </ol>
+      </nav>
+     </div>
+    </div>
+   </div>
+  </header>
+  <div class="container-fluid">
+   <div class="table-responsive">
+    <table class="table table-bordered">
+     <thead>
+      <tr>
+       <td>&nbsp;</td>
+       <td colspan="9"><div align="center"><strong>Code Coverage</strong></div></td>
+      </tr>
+      <tr>
+       <td>&nbsp;</td>
+       <td colspan="3"><div align="center"><strong>Lines</strong></div></td>
+       <td colspan="3"><div align="center"><strong>Functions and Methods</strong></div></td>
+       <td colspan="3"><div align="center"><strong>Classes and Traits</strong></div></td>
+      </tr>
+     </thead>
+     <tbody>
+      <tr>
+       <td class="warning">Total</td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="66.45" aria-valuemin="0" aria-valuemax="100" style="width: 66.45%">
+           <span class="sr-only">66.45% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">66.45%</div></td>
+       <td class="warning small"><div align="right">23434&nbsp;/&nbsp;35266</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="61.88" aria-valuemin="0" aria-valuemax="100" style="width: 61.88%">
+           <span class="sr-only">61.88% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">61.88%</div></td>
+       <td class="warning small"><div align="right">3742&nbsp;/&nbsp;6047</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="38.28" aria-valuemin="0" aria-valuemax="100" style="width: 38.28%">
+           <span class="sr-only">38.28% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">38.28%</div></td>
+       <td class="danger small"><div align="right">529&nbsp;/&nbsp;1382</div></td>
+      </tr>
+
+      <tr>
+       <td class="warning"><img src="_icons/file-directory.svg" class="octicon" /><a href="Channel/index.html">Channel</a></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="86.55" aria-valuemin="0" aria-valuemax="100" style="width: 86.55%">
+           <span class="sr-only">86.55% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">86.55%</div></td>
+       <td class="warning small"><div align="right">875&nbsp;/&nbsp;1011</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="81.86" aria-valuemin="0" aria-valuemax="100" style="width: 81.86%">
+           <span class="sr-only">81.86% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">81.86%</div></td>
+       <td class="warning small"><div align="right">167&nbsp;/&nbsp;204</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="52.08" aria-valuemin="0" aria-valuemax="100" style="width: 52.08%">
+           <span class="sr-only">52.08% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">52.08%</div></td>
+       <td class="warning small"><div align="right">25&nbsp;/&nbsp;48</div></td>
+      </tr>
+
+      <tr>
+       <td class="warning"><img src="_icons/file-directory.svg" class="octicon" /><a href="Pim/index.html">Pim</a></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="71.51" aria-valuemin="0" aria-valuemax="100" style="width: 71.51%">
+           <span class="sr-only">71.51% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">71.51%</div></td>
+       <td class="warning small"><div align="right">16010&nbsp;/&nbsp;22387</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="64.44" aria-valuemin="0" aria-valuemax="100" style="width: 64.44%">
+           <span class="sr-only">64.44% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">64.44%</div></td>
+       <td class="warning small"><div align="right">2338&nbsp;/&nbsp;3628</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="40.02" aria-valuemin="0" aria-valuemax="100" style="width: 40.02%">
+           <span class="sr-only">40.02% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">40.02%</div></td>
+       <td class="danger small"><div align="right">333&nbsp;/&nbsp;832</div></td>
+      </tr>
+
+      <tr>
+       <td class="danger"><img src="_icons/file-directory.svg" class="octicon" /><a href="Platform/index.html">Platform</a></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="41.99" aria-valuemin="0" aria-valuemax="100" style="width: 41.99%">
+           <span class="sr-only">41.99% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">41.99%</div></td>
+       <td class="danger small"><div align="right">1389&nbsp;/&nbsp;3308</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="48.25" aria-valuemin="0" aria-valuemax="100" style="width: 48.25%">
+           <span class="sr-only">48.25% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">48.25%</div></td>
+       <td class="danger small"><div align="right">275&nbsp;/&nbsp;570</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="39.53" aria-valuemin="0" aria-valuemax="100" style="width: 39.53%">
+           <span class="sr-only">39.53% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">39.53%</div></td>
+       <td class="danger small"><div align="right">68&nbsp;/&nbsp;172</div></td>
+      </tr>
+
+      <tr>
+       <td class="warning"><img src="_icons/file-directory.svg" class="octicon" /><a href="Tool/index.html">Tool</a></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="61.74" aria-valuemin="0" aria-valuemax="100" style="width: 61.74%">
+           <span class="sr-only">61.74% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">61.74%</div></td>
+       <td class="warning small"><div align="right">4105&nbsp;/&nbsp;6649</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="59.80" aria-valuemin="0" aria-valuemax="100" style="width: 59.80%">
+           <span class="sr-only">59.80% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">59.80%</div></td>
+       <td class="warning small"><div align="right">760&nbsp;/&nbsp;1271</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="34.06" aria-valuemin="0" aria-valuemax="100" style="width: 34.06%">
+           <span class="sr-only">34.06% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">34.06%</div></td>
+       <td class="danger small"><div align="right">94&nbsp;/&nbsp;276</div></td>
+      </tr>
+
+      <tr>
+       <td class="warning"><img src="_icons/file-directory.svg" class="octicon" /><a href="UserManagement/index.html">UserManagement</a></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="55.21" aria-valuemin="0" aria-valuemax="100" style="width: 55.21%">
+           <span class="sr-only">55.21% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">55.21%</div></td>
+       <td class="warning small"><div align="right">1055&nbsp;/&nbsp;1911</div></td>
+       <td class="warning big">       <div class="progress">
+         <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="54.01" aria-valuemin="0" aria-valuemax="100" style="width: 54.01%">
+           <span class="sr-only">54.01% covered (warning)</span>
+         </div>
+       </div>
+</td>
+       <td class="warning small"><div align="right">54.01%</div></td>
+       <td class="warning small"><div align="right">202&nbsp;/&nbsp;374</div></td>
+       <td class="danger big">       <div class="progress">
+         <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="16.67" aria-valuemin="0" aria-valuemax="100" style="width: 16.67%">
+           <span class="sr-only">16.67% covered (danger)</span>
+         </div>
+       </div>
+</td>
+       <td class="danger small"><div align="right">16.67%</div></td>
+       <td class="danger small"><div align="right">9&nbsp;/&nbsp;54</div></td>
+      </tr>
+
+
+     </tbody>
+    </table>
+   </div>
+   <footer>
+    <hr/>
+    <h4>Legend</h4>
+    <p>
+     <span class="danger"><strong>Low</strong>: 0% to 50%</span>
+     <span class="warning"><strong>Medium</strong>: 50% to 90%</span>
+     <span class="success"><strong>High</strong>: 90% to 100%</span>
+    </p>
+    <p>
+     <small>Generated by <a href="https://github.com/sebastianbergmann/php-code-coverage" target="_top">php-code-coverage 7.0.3</a> using <a href="https://secure.php.net/" target="_top">PHP 7.2.16-1+0~20190307202415.17+stretch~1.gbpa7be82</a> with <a href="https://github.com/krakjoe/pcov">PCOV 1.0.0</a> at Sun Mar 17 12:23:44 UTC 2019.</small>
+    </p>
+   </footer>
+  </div>
+ </body>
+</html>
